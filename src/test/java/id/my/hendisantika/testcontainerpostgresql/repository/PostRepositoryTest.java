a465b2f998d967dfe858b82dada824031b283214
hendisantika <hendisantika@yahoo.co.id>
2024-11-17 05:25:24 +0700

    Add PostRepositoryTest🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/testcontainerpostgresql/repository/PostRepositoryTest.java
@@ -0,0 +1,37 @@
+package id.my.hendisantika.testcontainerpostgresql.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-container-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/11/24
+ * Time: 05.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Testcontainers
+@DataJdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PostRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0");
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    JdbcConnectionDetails jdbcConnectionDetails;
+
+}
