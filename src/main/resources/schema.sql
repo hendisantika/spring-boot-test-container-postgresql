0ce4ab84383a5693770b6cd0773d2c76d3127a5e
hendisantika <hendisantika@yahoo.co.id>
2024-11-17 05:24:09 +0700

    Add schema.sql file🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE if NOT EXISTS Post
+(
+    id
+    INT
+    NOT
+    NULL,
+    user_id
+    INT
+    NOT
+    NULL,
+    title
+    VARCHAR
+(
+    250
+) NOT NULL,
+    body text NOT NULL,
+    version INT,
+    PRIMARY KEY
+(
+    id
+)
+    );
