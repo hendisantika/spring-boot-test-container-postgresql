c73216664d62690e60e56edb60818869c0deb75d
hendisantika <hendisantika@yahoo.co.id>
2024-11-17 05:34:43 +0700

    Add shouldCreateNewPostWhenPostIsValid🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/testcontainerpostgresql/SpringBootTestContainerPostgresqlApplicationTests.java
+++ src/test/java/id/my/hendisantika/testcontainerpostgresql/SpringBootTestContainerPostgresqlApplicationTests.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Objects;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -51,5 +55,19 @@ class SpringBootTestContainerPostgresqlApplicationTests {
     void shouldThrowNotFoundWhenInvalidPostID() {
         ResponseEntity<Post> response = restTemplate.exchange("/api/posts/999", HttpMethod.GET, null, Post.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    @Rollback
+    void shouldCreateNewPostWhenPostIsValid() {
+        Post post = new Post(101, 1, "101 Title", "101 Body", null);
+
+        ResponseEntity<Post> response = restTemplate.exchange("/api/posts", HttpMethod.POST, new HttpEntity<Post>(post), Post.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(Objects.requireNonNull(response.getBody()).id()).isEqualTo(101);
+        assertThat(response.getBody().userId()).isEqualTo(1);
+        assertThat(response.getBody().title()).isEqualTo("101 Title");
+        assertThat(response.getBody().body()).isEqualTo("101 Body");
     }
 }
