a78d0e762df75a64437b62223e6e08cd69bfe5a4
hendisantika <hendisantika@yahoo.co.id>
2024-11-17 05:33:26 +0700

    Modify Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/testcontainerpostgresql/SpringBootTestContainerPostgresqlApplicationTests.java
+++ src/test/java/id/my/hendisantika/testcontainerpostgresql/SpringBootTestContainerPostgresqlApplicationTests.java
@@ -1,13 +1,24 @@
 package id.my.hendisantika.testcontainerpostgresql;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 class SpringBootTestContainerPostgresqlApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0");
+
+    @Autowired
+    TestRestTemplate restTemplate;
 
 }
