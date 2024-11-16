7e9b5a8d8baefa28de7cee7251ff2891fc28efd5
hendisantika <hendisantika@yahoo.co.id>
2024-11-17 05:22:41 +0700

    Add findAll in PostController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/testcontainerpostgresql/controller/PostController.java
+++ src/main/java/id/my/hendisantika/testcontainerpostgresql/controller/PostController.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.testcontainerpostgresql.controller;
 
+import id.my.hendisantika.testcontainerpostgresql.model.Post;
 import id.my.hendisantika.testcontainerpostgresql.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +26,8 @@ class PostController {
 
     private final PostRepository repository;
 
+    @GetMapping("")
+    List<Post> findAll() {
+        return repository.findAll();
+    }
 }
