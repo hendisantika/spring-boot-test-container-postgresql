32c2d1e2db272bd32ee12283d20d353f1d4e8233
hendisantika <hendisantika@yahoo.co.id>
2024-11-17 05:29:58 +0700

    Add PostControllerTest🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/testcontainerpostgresql/controller/PostControllerTest.java
@@ -0,0 +1,36 @@
+package id.my.hendisantika.testcontainerpostgresql.controller;
+
+import id.my.hendisantika.testcontainerpostgresql.model.Post;
+import id.my.hendisantika.testcontainerpostgresql.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-test-container-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/11/24
+ * Time: 05.29
+ * To change this template use File | Settings | File Templates.
+ */
+@WebMvcTest(PostController.class)
+@AutoConfigureMockMvc
+class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    PostRepository repository;
+
+    List<Post> posts = new ArrayList<>();
+
+}
